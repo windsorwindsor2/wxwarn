3d5de6f6db7f199fe19b17ec7e1a93d2171106bb
Windsor <windsorwindsor2@protonmail.com>
2024-07-02 08:29:04 -0400

    Added states

--- wx.py
+++ wx.py
@@ -1,22 +1,24 @@
 import requests
 import os
 
-os.system('cls')
-
-url = "https://api.weather.gov/alerts/active?area=OK"
-
+'''
+pass a URL ot the function of the following format:
+https://api.weather.gov/alerts/active?area=OK
+Replace "OK" with any two-letter state abbreviation
+'''
+ 
 def get_wx_data(url):
     response = requests.get(url)
     return response.json()
 
-'''
-keys:
-'@context'
-'type'
-features = Bulk of data
-'title' - Title of requested data
-'updated'
-'''
+def state_url(state):
+    if len (state) != 2:
+        raise RuntimeError ("Use a valid 2-letter state abbreviation")
+    url = "https://api.weather.gov/alerts/active?area=" + state
+
+    return url
+
+
 def printmeta(alert):
     keys = ['@context', 'type', 'title', 'updated']
     for key in keys:
@@ -25,19 +27,34 @@ def printmeta(alert):
 def printkeys(alert):
     for i in alert:
         print (i)
-alert = get_wx_data(url)
-
-features = alert['features']
 
 '''
 keys:
+'@context'
+'type'
+features = Bulk of data
+'title' - Title of requested data
+'updated'
+
+keys in features:
 headline
 data
 description
 '''
-for i in features:
-    if 'Tornado Warning' in i['properties']['headline']:
-        print(i['properties']['headline'])
-        print(i['properties']['description'])
-        input('Press Enter to continue.')
-        os.system('cls')
+
+if __name__ == "__main__":
+    #Use Oklahoma as an example because tornados.
+    url = "https://api.weather.gov/alerts/active?area=OK"
+    
+    alert = get_wx_data(url)
+
+    features = alert['features']
+
+    os.system('cls')
+
+    for i in features:
+        if 'Tornado Warning' in i['properties']['headline']:
+            print(i['properties']['headline'])
+            print(i['properties']['description'])
+            input('Press Enter to continue.')
+            os.system('cls')
